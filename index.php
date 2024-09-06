6db4214b6950ec5eecb155f7e671877c4ff05a03
DevHamidreza <hamidrezashoshtari84@gmail.com>
2024-09-06 17:17:19 +0330

    add spam example

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
 
+use RateLimit\RateLimit;
+use RateLimit\SpamDetector;
+
 require_once 'vendor/autoload.php';
 
-$rateLimit = new Src\RateLimit();
+$rateLimit = new RateLimit();
+$spamDetector = new SpamDetector();
+
 $isLimited = $rateLimit->isLimited('index');
 
 if($isLimited){
     die('You are blocked');
+}
+
+$id = '12';
+$isSpam = $spamDetector->isSpam($id);
+
+if ($isSpam) {
+    die('Please do not spam' );
 }
 
 echo "welcome";
